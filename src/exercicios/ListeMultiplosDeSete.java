66fba4a4233dda75225bf6ac184cad998b55132c
Matheus Medeiros <mathmed22@gmail.com>
2022-07-08 19:06:12 -0300

    update ListeNumerosPares e criação do ListeMultiplosDeSete

--- src/exercicios/ListeMultiplosDeSete.java
+++ src/exercicios/ListeMultiplosDeSete.java
@@ -1,9 +1,9 @@
 package exercicios;
 
-public class ListeNumerosPares {
+public class ListeMultiplosDeSete {
     public static void main(String[] args) {
-        for(int i = 1; i < 1000; i++){
-            if(i % 2 == 0){
+        for (int i = 1; i < 1000; i++) {
+            if(i % 7 == 0){
                 System.out.println(i);
             }
         }
